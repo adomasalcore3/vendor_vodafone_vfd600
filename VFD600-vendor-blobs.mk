81d23eb6539aa21b95b0ea39e870db9b18c1a2d0
Vedat Ak <vedatak01@gmail.com>
2016-08-31 14:47:32 +0300

    Update VFD600-vendor-blobs.mk

--- VFD600-vendor-blobs.mk
+++ VFD600-vendor-blobs.mk
@@ -6,6 +6,6 @@ PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Vodafone/VFD600/pro
 
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Vodafone/VFD600/proprietary/vendor,system/vendor)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Vodafone/VFD600/proprietary/vendor/libtime_genoff.so,obj/lib/libtime_genoff.so)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Vodafone/VFD600/proprietary/vendor/lib/libtime_genoff.so,obj/lib/libtime_genoff.so)
 
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Vodafone/VFD600/proprietary/lib/modules/pronto/pronto_wlan.ko,system/lib/modules/pronto/pronto_wlan.ko)
