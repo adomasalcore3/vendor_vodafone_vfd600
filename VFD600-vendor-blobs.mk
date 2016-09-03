23414f9a10eb1681bedde61f2ba6d6012062afd3
Wzedlare <vedatak01@gmail.com>
2016-09-03 09:53:05 +0300

    Fix vendor blobs

--- VFD600-vendor-blobs.mk
+++ VFD600-vendor-blobs.mk
@@ -1,8 +1,14 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Vodafone/VFD600/proprietary/app,system/app)
+
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Vodafone/VFD600/proprietary/bin,system/bin)
 
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Vodafone/VFD600/proprietary/etc,system/etc)
 
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Vodafone/VFD600/proprietary/framework,system/framework)
+
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Vodafone/VFD600/proprietary/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Vodafone/VFD600/proprietary/priv-app,system/priv-app)
 
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Vodafone/VFD600/proprietary/vendor,system/vendor)
 
