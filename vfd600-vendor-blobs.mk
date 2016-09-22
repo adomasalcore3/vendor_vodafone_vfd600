aee965801ceea58629dd7b6dd4db23176e30270d
Vedat Ak <vedatak01@gmail.com>
2016-09-22 17:00:04 +0300

    Update vfd600-vendor-blobs.mk

--- vfd600-vendor-blobs.mk
+++ vfd600-vendor-blobs.mk
@@ -1,13 +1,13 @@
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Vodafone/vfd600/proprietary/app,system/app)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/vodafone/vfd600/proprietary/app,system/app)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Vodafone/vfd600/proprietary/bin,system/bin)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/vodafone/vfd600/proprietary/bin,system/bin)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Vodafone/vfd600/proprietary/etc,system/etc)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/vodafone/vfd600/proprietary/etc,system/etc)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Vodafone/vfd600/proprietary/framework,system/framework)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/vodafone/vfd600/proprietary/framework,system/framework)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Vodafone/vfd600/proprietary/lib,system/lib)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/vodafone/vfd600/proprietary/lib,system/lib)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Vodafone/vfd600/proprietary/priv-app,system/priv-app)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/vodafone/vfd600/proprietary/priv-app,system/priv-app)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Vodafone/vfd600/proprietary/vendor,system/vendor)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/vodafone/vfd600/proprietary/vendor,system/vendor)
